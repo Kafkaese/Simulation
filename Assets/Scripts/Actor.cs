91a241cea976977bcff617aa4986046c1f145669
Frederik Laubisch <kafkaese@gmail.com>
2021-11-04 12:56:38 +0100

    initial commit

---
+++ Assets/Scripts/Actor.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Actor : MonoBehaviour
+{
+
+    // Base Stats
+    private int _speed;
+
+    private int _strength;
+
+    private int _vision;
+
+    // Signs of Life
+    private int _health;
+
+    private int _hunger;
+
+    // Inventory
+    private List _inventory;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Eat(FoodItem food_item):
+
+}
